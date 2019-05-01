13fd3ccc2d066b9f221ea4fb68146ca13beea30e
Marina <taomarina@yandex.ru>
2019-05-01 14:27:04 +0300

    resolve 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
@@ -6,18 +7,41 @@ const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  const stream = fs.createReadStream(filepath);
 
   switch (req.method) {
     case 'GET':
 
-      break;
+      if (~pathname.search('/')) {
+        res.statusCode = 400;
+        res.end('400');
+      }
+    break;
 
     default:
       res.statusCode = 501;
       res.end('Not implemented');
   }
+
+  req.on('error', (err) => {
+    res.statusCode = 500;
+    res.end('500');
+  })
+
+  stream.on('data', (chunk) => {
+    res.write(chunk);
+  })
+
+  stream.on('end', () => {
+    res.statusCode = 200;
+    res.end();
+  })
+
+  stream.on('error', () => {
+    res.statusCode = 404;
+    res.end();
+  })
 });
 
 module.exports = server;
